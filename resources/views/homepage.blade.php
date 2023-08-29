0564f9a916779cdefcfe02b8948b6b4078e4aecb
Fahmii19 <fhmialfi79@gmail.com>
2023-08-29 19:06:45 +0700

    push

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.template')
 @section('content')
 
-<p class="border border-red-500">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsam dolor ratione quam est? Ullam magni praesentium a quaerat debitis est, delectus distinctio earum odio ad rerum modi harum nemo ex?</p>
+<p class="border border-red-500">Tailwinds x Vite template website sudah ready </p>
 
 @endsection
